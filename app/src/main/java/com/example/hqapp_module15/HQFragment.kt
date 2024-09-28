0a4aa794c88a4c1bf3062886cbe6b8ff66eb9ca0
Jusouza <jujussouza19@gmail.com>
2024-09-28 16:49:36 -0300

    Changes to complete the exercise adding AppBar With title and search bottom (without functions)

--- app/src/main/java/com/example/hqapp_module15/HQFragment.kt
+++ app/src/main/java/com/example/hqapp_module15/HQFragment.kt
@@ -6,10 +6,19 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navGraphViewModels
+import com.example.hqapp_module15.databinding.FragmentHqDetailsBinding
+import com.example.hqapp_module15.databinding.FragmentItemListBinding
 import com.example.hqapp_module15.placeholder.PlaceholderContent
 
 /**
@@ -20,10 +29,9 @@ class HQFragment : Fragment(), HQItemListener {
     private var columnCount = 1
 
     private val viewModel by navGraphViewModels<HQViewModel>(R.id.hq_nav_graph) { defaultViewModelProviderFactory }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setHasOptionsMenu(true)
         arguments?.let {
             columnCount = it.getInt(ARG_COLUMN_COUNT)
         }
@@ -33,19 +41,30 @@ class HQFragment : Fragment(), HQItemListener {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_item_list, container, false)
+        val binding = FragmentItemListBinding.inflate(inflater, container, false)
 
-        // Set the adapter
-        if (view is RecyclerView) {
-            with(view) {
-                layoutManager = when {
-                    columnCount <= 1 -> LinearLayoutManager(context)
-                    else -> GridLayoutManager(context, columnCount)
-                }
-                adapter = MyhqRecyclerViewAdapter(PlaceholderContent.ITEMS, this@HQFragment)
-            }
+        // Setup the toolbar
+        val toolbar: Toolbar = binding.toolbar // Certifique-se de que você tenha uma Toolbar no seu layout
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+
+        // Set the adapter for RecyclerView
+        binding.list.layoutManager = when {
+            columnCount <= 1 -> LinearLayoutManager(context)
+            else -> GridLayoutManager(context, columnCount)
         }
-        return view
+
+        binding.list.layoutManager = when {
+            columnCount <= 1 -> LinearLayoutManager(context)
+            else -> GridLayoutManager(context, columnCount)
+        }
+        binding.list.adapter = MyhqRecyclerViewAdapter(PlaceholderContent.ITEMS, this)
+
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu, menu)
     }
 
     override fun onItemSelected(position: Int) {
